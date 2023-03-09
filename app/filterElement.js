6c6fc993dd8c10b4c3732e45bda52b3c7f23f8da
gabriel <gabriel.f.lazari@gmail.com>
2023-03-09 17:16:22 -0300

    filtrando

---
+++ app/filterElement.js
@@ -0,0 +1,12 @@
+const allFilter = document.querySelectorAll('[categoria]');
+
+allFilter.forEach(categoria => {
+    categoria.addEventListener('click', () => {
+        filtrarCategoria(categoria.value);
+    })
+})
+
+function filtrarCategoria(categoria) {
+    let livrosFiltrados = livros.filter(x => x.categoria == categoria);
+    console.table(livrosFiltrados);
+}
